8041a8946003dec858a501ccb1c7b43e180a866d
RahulRishavMohanti <rahul.spikes@gmail.com>
2019-05-31 03:35:15 +0530

    Updates

---
+++ precache-manifest.c972e71e70e68a250131d6f7bea92042.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31cb42814fa6c687d15b77cccc29691d",
+    "url": "/portfol/index.html"
+  },
+  {
+    "revision": "121b1c75625cd06e24cb",
+    "url": "/portfol/static/css/main.caed88ed.chunk.css"
+  },
+  {
+    "revision": "1e652ac2005756ae95d1",
+    "url": "/portfol/static/js/2.7254bb26.chunk.js"
+  },
+  {
+    "revision": "121b1c75625cd06e24cb",
+    "url": "/portfol/static/js/main.cf89a631.chunk.js"
+  },
+  {
+    "revision": "ac970dd0a81979c9dab9",
+    "url": "/portfol/static/js/runtime~main.6326cbec.js"
+  },
+  {
+    "revision": "3f0cf4522b43cf70aac0782e2605a62d",
+    "url": "/portfol/static/media/RM.3f0cf452.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfol/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "32483a7439672ff8553b4eeb3082ff3a",
+    "url": "/portfol/static/media/sk.32483a74.jpg"
+  }
+]);
